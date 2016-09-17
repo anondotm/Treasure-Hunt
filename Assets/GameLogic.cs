10e7e7cf7f71600763fdc3164d97edfa68593ffe
Anan McHarris <am7155@nyu.edu>
2016-09-16 23:03:35 -0400

    First commit for Treasure Hunt project. Basic player controls and logic system (with non functioning victory message). Includes prototype sattelite model.

---
+++ Assets/GameLogic.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour {
+	bool didPlayerWin = false;
+	public Text controlPrompt;
+	public Text textObject;
+	public string[] treasureString; //array of messages for "textObject"
+	public Transform player;
+	public Transform treasureActive;
+	public Transform[] treasureArray; //array of treasure Transforms
+	public int counter; //counter variable
+	// Use this for initialization
+	void Start () {
+		counter = 0;
+		treasureActive = treasureArray [counter];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (didPlayerWin) {
+				textObject.text = "You can leave now!";
+		} 
+		else if ((player.position - treasureActive.position).magnitude < 5f) {
+			//controlPrompt.SetActive (true);
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				
+				if (counter >= treasureArray.Length) {//if you have reached the last treasure
+					//didPlayerWin = true;
+					Debug.Log("You can go back!");
+				} 
+
+				else { //otherwise, add another one on!
+					counter++;
+				}
+			}
+		}
+
+		if (didPlayerWin == false) {
+			textObject.text = treasureString [counter];
+		}
+		}
+	}
---
+++ Assets/playerScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerScript : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position += new Vector3 (0, 0, speed) * Time.deltaTime;
+			transform.eulerAngles = new Vector3 (0, 0, 1);
+		}
+		else if (Input.GetKey (KeyCode.S)) {
+			transform.position += new Vector3 (0, 0, -speed) * Time.deltaTime;
+			transform.eulerAngles = new Vector3 (0, 0, -1);
+		}
+		else if (Input.GetKey (KeyCode.A)) {
+			transform.position += new Vector3 (-speed, 0, 0) * Time.deltaTime;
+			transform.eulerAngles = new Vector3 (-1, 0, 0);
+		}
+		else if (Input.GetKey (KeyCode.D)) {
+			transform.position += new Vector3 (speed, 0, 0) * Time.deltaTime;
+			transform.eulerAngles = new Vector3 (1, 0, 0);
+		}
+		Camera.main.transform.position = transform.position + new Vector3 (0, 2, -4);
+	}
+}
