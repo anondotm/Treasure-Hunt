375b687ca73aaa1d9b57f7413fb5da7cd7da86f5
Anan McHarris <am7155@nyu.edu>
2016-09-19 21:33:20 -0400

    Resolved the "turning issue" for the player-character (thank you charles and jack!)

---
+++ Assets/bodyScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bodyScript : MonoBehaviour {
+	public GameObject[] torso;
+	public Vector3 rotateAmount;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.W)) {
+			transform.Rotate(0, 1, 0);
+//			for (int i = 0; i < torso.Length; i++) {
+//				torso [i].transform.Rotate (rotateAmount.x, rotateAmount.y, rotateAmount.z);
+//			}
+		}
+		else if (Input.GetKeyDown (KeyCode.S)) {
+			rotateAmount = new Vector3 (0, -1, 0);
+//			for (int i = 0; i < torso.Length; i++) {
+				//torso [i].transform.localEulerAngles = transform.eulerAngles;
+//				torso [i].transform.Rotate (rotateAmount.x, rotateAmount.y, rotateAmount.z);
+//			}
+		}
+		else if (Input.GetKeyDown (KeyCode.A)) {
+			transform.Rotate(-1, 0, 0);
+//			for (int i = 0; i < torso.Length; i++) {
+//				//torso [i].transform.localEulerAngles = transform.eulerAngles;
+//				torso [i].transform.Rotate (rotateAmount.x, rotateAmount.y, rotateAmount.z);
+//			}
+		}
+		else if (Input.GetKeyDown (KeyCode.D)) {
+			transform.Rotate(1, 0, 0);
+//			for (int i = 0; i < torso.Length; i++) {
+//				//torso [i].transform.localEulerAngles = transform.eulerAngles;
+//				torso [i].transform.Rotate (rotateAmount.x, rotateAmount.y, rotateAmount.z);
+//			}
+		}
+	}
+}
---
+++ Assets/buttonScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			transform.eulerAngles = new Vector3 (0, 1, 0);
+		}
+		else if (Input.GetKey (KeyCode.S)) {
+			transform.eulerAngles = new Vector3 (0, -1, 0);
+		}
+		else if (Input.GetKey (KeyCode.A)) {
+			transform.eulerAngles = new Vector3 (-1, 0, 0);
+		}
+		else if (Input.GetKey (KeyCode.D)) {
+			transform.eulerAngles = new Vector3 (1, 0, 0);
+		}
+	}
+}
